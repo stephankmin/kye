3b4df3e0cc3393efb43dcabaef5a147f5ec0b654
stephankmin <33536922+stephankmin@users.noreply.github.com>
2021-11-23 21:21:27 -0800

    reformatted tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -5,6 +5,8 @@ const { ethers } = require("hardhat");
 
 describe("Kye", () => {
   let kyeContract;
+  let kye;
+  let kyeDeployed;
   let deployer;
   let accounts;
   let user1;
@@ -16,33 +18,41 @@ describe("Kye", () => {
     user1 = accounts[1];
     kyeContract = await ethers.getContractFactory("Kye");
     depositAmount = ethers.utils.parseEther("1.0");
-    kye = await kyeContract.deploy(ethers.utils.parseEther("1.0"),7,10);
-    await kye.deployed();
   });
 
   describe("constructor", () => {
+    before(async () => {
+      kye = await kyeContract.deploy(ethers.utils.parseEther("1.0"),7,10);
+      kyeDeployed = await kye.deployed();
+    });
+
     it("should return depositAmount passed into constructor", async function () {
-      expect(await kye.depositAmount()).to.equal(ethers.utils.parseEther("1.0"));
+      expect(await kyeDeployed.depositAmount()).to.equal(ethers.utils.parseEther("1.0"));
     });
   
     it("should return lengthOfRound passed into constructor", async function () {
-      expect(await kye.lengthOfRound()).to.equal(7);
+      expect(await kyeDeployed.lengthOfRound()).to.equal(7);
     });
 
     it("should return requiredNumberOfUsers passed into constructor", async function () {
-      expect(await kye.requiredNumberOfUsers()).to.equal(10);
+      expect(await kyeDeployed.requiredNumberOfUsers()).to.equal(10);
     });
   });
 
   describe("startKye()", () => {
+    before(async () => {
+      kye = await kyeContract.deploy(ethers.utils.parseEther("1.0"),7,10);
+      kyeDeployed = await kye.deployed();
+    });
+
     it("should be reverted if called by non-owner", async function () {
-      await expect(kye.connect(user1).startKye())
+      await expect(kyeDeployed.connect(user1).startKye())
         .to.be.revertedWith("Only the contract owner can call this function");
     });
 
     it("should set isActive to true", async function () {
-      await (await kye.startKye()).wait();
-      expect(await kye.isActive()).to.equal(true);
+      await (await kyeDeployed.startKye()).wait();
+      expect(await kyeDeployed.isActive()).to.equal(true);
     });
 
     it("should set firstRoundInProgress to true", async function () {
@@ -54,36 +64,33 @@ describe("Kye", () => {
       const block = await ethers.provider.getBlock(blockNum);
       const timestamp = block.timestamp;
 
-      expect(await kye.lastDistribution()).to.equal(timestamp);
+      expect(await kyeDeployed.lastDistribution()).to.equal(timestamp);
     });
   }); 
 
   describe("addUser()", () => {
     before(async () => {
-      accounts = await ethers.getSigners();
-      deployer = accounts[0];
-      user1 = accounts[1];
-      kyeContract = await ethers.getContractFactory("Kye");
-      depositAmount = ethers.utils.parseEther("1.0");
       kye = await kyeContract.deploy(ethers.utils.parseEther("1.0"),7,10);
-      await kye.deployed();
+      kyeDeployed = await kye.deployed();
     });
 
     it("should be reverted if isActive is false", async function () {
-      await expect(kye.addUser(user1.address))
+      await expect(kyeDeployed.addUser(user1.address))
         .to.be.revertedWith("Kye has not started yet");
     });
 
     it("should be reverted if called by non-owner", async function () {
-      await (await kye.startKye()).wait();
+      const startTxn = await kye.startKye();
+      const startTxnComplete = await startTxn.wait();
 
       const newUser = accounts[2];
-      await expect(kye.connect(user1).addUser(newUser.address))
+      await expect(kyeDeployed.connect(user1).addUser(newUser.address))
         .to.be.revertedWith("Only the contract owner can call this function");
     });
 
     it("should set isUser for user1 to true", async function () {
-      await (await kye.addUser(user1.address)).wait();
+      const addUserTxn = await kye.addUser(user1.address)
+      const addUserTxnComplete = await addUserTxn.wait();
       const userStruct = await kye.users(user1.address);
 
       expect(userStruct['isUser']).to.equal(true);
@@ -104,13 +111,8 @@ describe("Kye", () => {
 
   describe("deposit()", () => {
     before(async () => {
-      accounts = await ethers.getSigners();
-      deployer = accounts[0];
-      user1 = accounts[1];
-      kyeContract = await ethers.getContractFactory("Kye");
-      depositAmount = ethers.utils.parseEther("1.0");
       kye = await kyeContract.deploy(ethers.utils.parseEther("1.0"),7,10);
-      await kye.deployed();
+      kyeDeployed = await kye.deployed();
     });
 
     it("should be reverted if isActive is false", async function () {
@@ -119,7 +121,8 @@ describe("Kye", () => {
     });
     
     it("should be reverted if called by non-user", async function () {
-      await (await kye.startKye()).wait();
+      const startTxn = await kye.startKye();
+      const startTxnComplete = startTxn.wait();
       await kye.addUser(user1.address);
 
       const nonUser = accounts[2];
@@ -164,15 +167,12 @@ describe("Kye", () => {
 
   describe("distributePool()", () => {
     beforeEach(async () => {
-      accounts = await ethers.getSigners();
-      deployer = accounts[0];
-      user1 = accounts[1];
-      kyeContract = await ethers.getContractFactory("Kye");
-      depositAmount = ethers.utils.parseEther("1.0");
-      kye = await kyeContract.deploy(ethers.utils.parseEther("1.0"),7,10);
-      await kye.deployed();
 
-      await (await kye.startKye()).wait();
+      kye = await kyeContract.deploy(ethers.utils.parseEther("1.0"),7,10);
+      kyeDeployed = await kye.deployed();
+
+      const startTxn = await kye.startKye();
+      const startTxnComplete = await startTxn.wait();
 
       for(const a of accounts.slice(1,11)) {
         await (await kye.addUser(a.address)).wait();
@@ -198,8 +198,8 @@ describe("Kye", () => {
         await kye.connect(a).deposit({ value: depositAmount });
       }
 
-      await network.provider.send("evm_increaseTime", [604800]);
-      await network.provider.send("evm_mine");
+      const incTime = await network.provider.send("evm_increaseTime", [604800]);
+      const mineBlock = await network.provider.send("evm_mine");
 
       const distribution = await kye.connect(user1).distributePool();
       const user1Struct = await kye.users(user1.address); 
